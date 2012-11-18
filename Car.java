fb88ce3e35f5fc158edd9222d7d73aa4b0dffc78
Thomas Davids <tfdavids@gmail.com>
2012-11-18 13:15:02 -0800

    First draft of working simulator

--- Car.java
+++ Car.java
@@ -1,29 +1,42 @@
 
 public class Car {
-	
+
 	private int street;
 	private double speed;
 	private double position;
 	private City city;
-	public static final double MAX_SPEED = 45;
-	
+	private int steps;
+
+	// Parameters for traffic flow
+	public static final double MAX_SPEED = 30.0;
+	public static final double SAFE_TIME = 1.5;
+	public static final double MAX_ACCEL = 1.0;
+	public static final double MAX_DECEL = 3.0;
+	public static final int ACCEL_EXP = 4;
+	public static final double JAM_DIST = 2.0;
+	public static final double VEHICLE_LEN = 5.0;
+
+
 	@Override
 	public String toString() {
-		return "Car [street=" + street + ", speed=" + speed + ", position="
-				+ position + ", city=" + city + "]";
+		/*return "Car [street=" + street + ", speed=" + speed + ", position="
+				+ position + ", city=" + city + "]";*/
+		return "Car [street=" + street + ", average_speed=" + (position / steps) + "]";
 	}
 
-	public Car(City city, int street) {
+	public Car(City city, int street, double speed) {
 		this.city = city;
 		this.street = street;
-		this.speed = MAX_SPEED; // this may need to be adjusted -- speed of car immediately in front of it?
+		this.speed = speed; // this may need to be adjusted -- speed of car immediately in front of it?
 		this.position = 0.0;
+		this.steps = 0;
 	}
-	
+
 	public void step() {
 		// TODO: finish implementing
-
+		
 		position += speed;
+
 		if (street % 2 == 0) {
 			if (position > City.HORIZONTAL_STREET_LENGTH) {
 				city.removeCar(this, street);
@@ -33,20 +46,63 @@ public class Car {
 				city.removeCar(this, street);
 			}
 		}
+
+		Car nextVehicle = getLeader();
+		Light nextLight = city.getNextLight(street, position, speed);
+		double nextLightPosition = city.getNextLightPosition(street, position, speed);
+		
+		if (nextVehicle == null && nextLight == null) {
+			// no interaction component if we can't see another car
+			
+			double freeComponent = MAX_ACCEL * (1.0 - Math.pow((speed / MAX_SPEED), 4));
+
+			speed += freeComponent;
+			
+		} else if ((nextVehicle == null && nextLight != null) ||
+				(nextVehicle != null && nextLightPosition < nextVehicle.getPosition())) {
+			// treat the light like a stop sign
+			
+			double currentDistance = nextLightPosition - position;
+			double approachingRate = speed - 0;
+
+			double freeComponent = MAX_ACCEL * (1.0 - Math.pow((speed / MAX_SPEED), 4));
+			double intComponent = -MAX_ACCEL * Math.pow((JAM_DIST + speed * SAFE_TIME) / currentDistance + speed * approachingRate / (2.0 * Math.pow(MAX_ACCEL * MAX_DECEL, 0.5) * currentDistance), 2);
+
+			speed += freeComponent + intComponent;
+			
+			//System.err.println("Slowing for light on street " + street);
+			
+		} else if ((nextVehicle != null && nextLight == null) ||
+			(nextVehicle != null && nextLightPosition > nextVehicle.getPosition())) {
+
+			double currentDistance = nextVehicle.getPosition() - position;
+			double approachingRate = speed - nextVehicle.getSpeed();
+
+			double freeComponent = MAX_ACCEL * (1.0 - Math.pow((speed / MAX_SPEED), 4));
+			double intComponent = -MAX_ACCEL * Math.pow((JAM_DIST + speed * SAFE_TIME) / currentDistance + speed * approachingRate / (2.0 * Math.pow(MAX_ACCEL * MAX_DECEL, 0.5) * currentDistance), 2);
+
+			speed += freeComponent + intComponent;
+		} 
+
+		steps++;
 		
 	}
-	
+
 	public double getPosition() {
 		return this.position;
+	}
+
+	public double getSpeed() {
+		return this.speed;
 	}
 	
 	public City getCity() {
 		return this.city;
 	}
-	
+
 	// get the car in front of this one
 	private Car getLeader() {
 		return this.city.getLeader(this, street);
 	}
-	
+
 }
--- City.java
+++ City.java
@@ -2,28 +2,101 @@ import java.util.*;
 
 public class City {
 	// EVEN STREETS ARE HORIZONTAL (0-indexed), ODD STREETS ARE VERTICAL
-	
+
 	public static final int HORIZONTAL_STREETS = 2;
 	public static final int VERTICAL_STREETS = 2;
-	
+
 	public static final double HORIZONTAL_STREET_LENGTH = 1000.0; // in meters
 	public static final double VERTICAL_STREET_LENGTH = 1000.0; // in meters
-	
+
+	public static final double CAR_GEN_PROB = 0.25;
+
 	private Vector<Vector<Car>> cars;
-	private Vector<Light> stoplights; // lights are numbered left to right, top to bottom
-	
+	private Vector<Vector<Light>> stoplights; // first index of light is vertical street (x-coord), second is horizontal street
+
+	Random rand = new Random();
+
 	public City () {
 		// TODO: Fill this in
-		
+
 		this.cars = new Vector<Vector<Car>>();
 		for (int i = 0; i < HORIZONTAL_STREETS + VERTICAL_STREETS; i++) {
 			this.cars.add(new Vector<Car>());
-			this.cars.elementAt(i).add(new Car(this, i));
 		}
-		
-		this.stoplights = new Vector<Light>();
+
+		this.stoplights = new Vector<Vector<Light>>();
+		for (int i = 0; i < VERTICAL_STREETS; i++) {
+			this.stoplights.add(new Vector<Light>());
+			for (int j = 0; j < HORIZONTAL_STREETS; j++) {
+				this.stoplights.elementAt(i).add(new Light());
+			}
+		}
 	}
 	
+	public Light getLight(int street1, int street2) {
+		if (street1 % 2 == street2 % 2) {
+			System.err.println("Error: trying to find intersection of parallel streets");
+			return null;
+		}
+		
+		if (street1 % 2 == 0) {
+			return this.stoplights.elementAt((street2 - 1) / 2).elementAt(street1 / 2);
+		} else {
+			return this.stoplights.elementAt((street1 - 1) / 2).elementAt(street2 / 2);
+		}
+		
+	}
+	
+	public double getLightPosition(int street, int cross) {
+		if (street % 2 == 0) {
+			return (((double) cross - 1) / 2) / (VERTICAL_STREETS + 1);
+		} else {
+			return ((double) cross / 2) / (HORIZONTAL_STREETS + 1);
+		}
+	}
+	
+	public Light getNextLight(int street, double position, double speed) {
+		// returns the next light that will affect the travel of a car at this location and speed
+		// NOTE: if a car can stop at a yellow light (or red light) it does, otherwise continues on
+		
+		// this method assumes we're going from low-indexed streets to high-indexed streets
+		
+		double stoppingDistance = speed * speed / (2 * Car.MAX_DECEL);
+		
+		if (street % 2 == 1) {
+			for (int i = 0; i < stoplights.elementAt((street - 1) / 2).size(); i++) {
+				if (getLightPosition((street - 1) / 2, 2 * i) > position + stoppingDistance)
+					return stoplights.elementAt((street - 1) / 2).elementAt(i);
+			}
+		} else {
+			for (int i = 0; i < stoplights.size(); i++) {
+				if (getLightPosition(street / 2, 2 * i + 1) > position + stoppingDistance)
+					return stoplights.elementAt(i).elementAt(street / 2);
+			}
+		}
+		
+		return null;
+	}
+	
+	public double getNextLightPosition(int street, double position, double speed) {
+		double stoppingDistance = speed * speed / (2 * Car.MAX_DECEL);
+		
+		if (street % 2 == 1) {
+			for (int i = 0; i < stoplights.elementAt((street - 1) / 2).size(); i++) {
+				if (getLightPosition((street - 1) / 2, 2 * i) > position + stoppingDistance)
+					return getLightPosition((street - 1) / 2, 2 * i);
+			}
+		} else {
+			for (int i = 0; i < stoplights.size(); i++) {
+				if (getLightPosition(street / 2, 2 * i + 1) > position + stoppingDistance)
+					return getLightPosition(street / 2, 2 * i + 1);
+			}
+		}
+		
+		return -1;
+	}
+	
+/*
 	public Light.LightState getLightState(int street, int index) {
 		if (street % 2 == 0) {
 			return stoplights.elementAt(street / 2 + index).getState(street);
@@ -31,25 +104,48 @@ public class City {
 			return stoplights.elementAt(index * VERTICAL_STREETS + (street - 1) / 2).getState(street);
 		}
 	}
-	
+*/
 	public void step() {
-		
+
 		// First step with each car
 		for (int street = 0; street < this.cars.size(); street++) {
 			for (int car = 0; car < this.cars.elementAt(street).size(); car++) {
 				this.cars.elementAt(street).elementAt(car).step();
 			}
 		}
-		
+
 		// Now potentially add new cars
-		
-		
+		for (int street = 0; street < this.cars.size(); street++) {
+			if (this.cars.elementAt(street).size() > 0) {
+				Car firstCar = this.cars.elementAt(street).lastElement();
+
+				if (firstCar.getPosition() > Car.VEHICLE_LEN + 10.0) {
+					// TODO: fix the conditional here
+
+					double x = rand.nextDouble();
+					if (x < CAR_GEN_PROB) {
+						this.cars.elementAt(street).add(new Car(this, street, firstCar.getSpeed()));
+						System.out.println("Added car: " + this.cars.elementAt(street).lastElement().toString());
+					}
+				}
+			} else {
+				double x = rand.nextDouble();
+				if (x < CAR_GEN_PROB) {
+					this.cars.elementAt(street).add(new Car(this, street, Car.MAX_SPEED));
+					System.out.println("Added car: " + this.cars.elementAt(street).lastElement().toString());
+				}
+			}
+		}
+
 		// Then let each light make its decision
-		for (int light = 0; light < this.stoplights.size(); light++) {
-			this.stoplights.elementAt(light).step();
+		for (int i = 0; i < stoplights.size(); i++) {
+			for (int j = 0; j < stoplights.elementAt(i).size(); j++) {
+				stoplights.elementAt(i).elementAt(j).step();
+			}
 		}
 	}
-	
+
+	// Returns the car directly in front of this one, on the given street
 	public Car getLeader(Car car, int street) {
 		int index = cars.elementAt(street).indexOf(car);
 		if (index < cars.elementAt(street).size() - 1) {
@@ -58,12 +154,12 @@ public class City {
 			return null;
 		}
 	}
-	
+
 	public void removeCar(Car car, int street) {
 		cars.elementAt(street).remove(car);
 		System.out.println("Removed car: " + car.toString());
 	}
-	
+
 	public void simulate() {
 		while (true) {
 			step();
@@ -72,7 +168,7 @@ public class City {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.err.println("Slept!");
+			//System.err.println("Slept!");
 		}
 	}
 }
