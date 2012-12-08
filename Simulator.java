58c5178947ea4d9d913166b9228526097ef96b70
Luke Knepper <luke@knepper.com>
2012-12-08 13:54:13 -0800

    FVI #1
    Fitted Value Iteration, draft 1

--- Simulator.java
+++ Simulator.java
@@ -9,10 +9,14 @@ public class Simulator {
 		double constant = city.simulate(Light.LightMethod.CONSTANT);
 		city = new City();
 		double regression = city.simulate(Light.LightMethod.REGRESSION);
+//		city = new City();
+		// double fvi = city.simulate(Light.LightMethod.FVI);
+		
 		
 		System.out.println("Average speed for regression was " + regression + " meters per second.");
 		System.out.println("Average speed for benchmark (constant) was " + constant + " meters per second.");
 		System.out.println("Learning algorithm saved " + (regression-constant) + " meters per second.");
+		//System.out.println("Average speed for FVI was " + fvi + " meters per second.");
 	}
 
 }
